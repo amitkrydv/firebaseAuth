62a5f9dbeb4d1e45081a95227d1f8af14905f880
Amit Kumar <amityadav46613.img@gmail.com>
2023-04-12 14:21:26 +0530

    firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJxBpylQ6cQodpgiqtAJdT6MQPV2bexD0",
+  authDomain: "emailauth-8a9ed.firebaseapp.com",
+  projectId: "emailauth-8a9ed",
+  storageBucket: "emailauth-8a9ed.appspot.com",
+  messagingSenderId: "429679844186",
+  appId: "1:429679844186:web:508e5217541e31ed03f163"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export default app;
